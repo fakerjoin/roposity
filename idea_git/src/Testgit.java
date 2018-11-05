1b38983e82d62ae781466baef389308f71687cee
shi <844986634@qq.com>
2018-11-05 19:27:01 +0800

    添加test工程到git

---
+++ idea_git/src/Testgit.java
@@ -0,0 +1,8 @@
+public class Testgit {
+    public static void main(String[] args) {
+        System.out.println("heheheh");
+        System.out.println("hihihih");
+        System.out.println("hahahah");
+        System.out.println("yeyeyey");
+    }
+}
